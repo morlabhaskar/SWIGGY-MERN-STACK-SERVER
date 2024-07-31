5c361bdfaa93e8a693883a2f0ea10887020eb074
morlabhaskar <morlabhaskar306@gmail.com>
2024-07-31 13:53:38 +0530

    modified

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
-// app.use(cors())
+app.use(cors())
 // app.use(
 //     cors({
 //         origin: ["http://localhost:5173"],
